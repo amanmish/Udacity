bd48f706f9697cfaea036789f025b925fa653f17
Aman Mishra <amanmishracse@gmail.com>
2018-11-10 20:37:00 +0530

    Add files via upload

---
+++ Restaurant-solution/sw.js
@@ -0,0 +1,39 @@
+// install cache
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open("restaurant_cache").then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/restaurant.html',
+        '/style.css',
+        '/js/dbhelper.js',
+        '/js/main.js',
+        '/js/restaurant_info.js',
+        '/data/restaurants.json',
+        '/img/1.jpg',
+        '/img/2.jpg',
+        '/img/3.jpg',
+        '/img/4.jpg',
+        '/img/5.jpg',
+        '/img/6.jpg',
+        '/img/7.jpg',
+        '/img/8.jpg',
+        '/img/9.jpg',
+        '/img/10.jpg'
+      ]);
+    })
+  );
+});
+// activate cache
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+// fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
